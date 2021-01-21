37021db9b9956c9e03503a6b31ee57ba7a38cfbd
Vladislav <vlad.tiw27@gmail.com>
2021-01-22 00:54:46 +0200

    Добавил для секции "Showcase" разметку стили и скрипты, тоже самое сделал для секции "Partners" только без скриптов, и сделал наброски секции "Recipes"  добавил немного разметки, стилей и скриптов.

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,10 @@
 const menu = ['Slide 1', 'Slide 2', 'Slide 3']
 const mySwiper = new Swiper ('.swiper-container', {
     // If we need pagination
+    loop: true,
+    autoplay: {
+      delay: 3000
+    },
     pagination: {
       el: '.swiper-pagination',
 			clickable: true,
@@ -14,4 +18,20 @@ const mySwiper = new Swiper ('.swiper-container', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  })
+  });
+
+const showCase = new Swiper('.showcase__slider', {
+  slidesPerView: 5,
+  spaceBetween: 1,
+  loop: true,
+  autoplay: {
+    delay: 1200
+  }
+});
+
+const recipes = new Swiper('.recipes__slider', {
+  slidesPerView: 'auto',
+  loop: true,
+  centeredSlides: true,
+  spaceBetween: 30
+});
